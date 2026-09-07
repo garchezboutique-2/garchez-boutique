5019f0a2456795f82f0e8c704efe9eec3e8af874
garchezboutique-2 <garchezboutique@gmail.com>
2026-09-07 13:50:21 -0600

    Add categories configuration for products

---
+++ src/config/categories.ts
@@ -0,0 +1,12 @@
+// Categorías de productos
+const categories = [
+  "Ropa de Dama",
+  "Ropa de Caballero",
+  "Accesorios",
+  "Calzado",
+  "Bolsos",
+  "Joyería",
+  "Ofertas"
+];
+
+export default categories;
