855120b1e936fcef4e46e282086a34dbe029b977
garchezboutique-2 <garchezboutique@gmail.com>
2026-09-07 14:38:14 -0600

    Add TypeScript types to addToast and removeToast

--- src/lib/toast.ts
+++ src/lib/toast.ts
@@ -3,10 +3,10 @@ import { atom } from 'nanostores';
 // Almacenamiento para notificaciones (toasts)
 export const toastStore = atom([]);
 
-export const addToast = (message) => {
+export const addToast = (message: string) => {
   toastStore.set([...toastStore.get(), { message, id: Date.now() }]);
 };
 
-export const removeToast = (id) => {
+export const removeToast = (id: number) => {
   toastStore.set(toastStore.get().filter(toast => toast.id !== id));
 };
