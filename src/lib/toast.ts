16887921155d7af269af9867718ee88610e9b55f
garchezboutique-2 <garchezboutique@gmail.com>
2026-09-07 13:59:48 -0600

    Add toast notification store and functions

---
+++ src/lib/toast.ts
@@ -0,0 +1,12 @@
+import { atom } from 'nanostores';
+
+// Almacenamiento para notificaciones (toasts)
+export const toastStore = atom([]);
+
+export const addToast = (message) => {
+  toastStore.set([...toastStore.get(), { message, id: Date.now() }]);
+};
+
+export const removeToast = (id) => {
+  toastStore.set(toastStore.get().filter(toast => toast.id !== id));
+};
